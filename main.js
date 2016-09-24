274b223b9b5114080a6046f54dcff46f1813064c
Rahul Barahpuria <rahul.rb553@gmail.com>
2016-09-24 11:20:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 const{app, BrowserWindow, Tray} = require('electron');
-//var updater = require('electron-updater');
-
 let win;
-//console.log('the Application');
-//console.log(process.versions['electron']);
 function createWindow(){
-  //updater.on('ready', function(){
     win = new BrowserWindow({
       height : 800,
       width : 600
 
       });
-    win.loadURL('C:/rahul/Electron/HelloWorld/index.html');
-
-//  });
-
-//  updater.on('updateRequired', function () {
-//        app.quit();
-//  });
-//  updater.on('updateAvailable', function () {
-//        mainWindow.webContents.send('update-available');
-//  });
-//  updater.start();
+    win.loadURL('C:/rahul/Electron/HelloWorld/index.html');//you can choose your own path
 }
 app.on('ready', createWindow);
